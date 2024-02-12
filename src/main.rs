723148e39cd4b755c9b84471b5e26b816c1bc42b
pdol <79060962+gm015@users.noreply.github.com>
2024-02-12 01:36:50 +0100

    add callback //alias

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ slint::slint!{
     import { Button, VerticalBox } from "std-widgets.slint";
     export component App inherits Window {
         property <int> counter: 2;
+        callback clicked <=> btn.clicked;   //alias
         VerticalBox {
             Text { text: "Hello power of 2: " + counter; }
-            Button {text: "yay"; clicked => {counter *= 2}}
+            btn := Button {text: "yay"; }
         }
     }
 }
