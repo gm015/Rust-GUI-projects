87a3ac90e46afc39677671c264282f4616f8597c
pdol <79060962+gm015@users.noreply.github.com>
2024-02-12 02:01:34 +0100

    update button function + add alias to callback and remove logic from slint + add logic into rust code: main()

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use slint::Weak;
+
 slint::slint!{
     import { Button, VerticalBox } from "std-widgets.slint";
     export component App inherits Window {
-        property <int> counter: 2;
+        in property <int> counter: 2;        // in -> make it public
         callback clicked <=> btn.clicked;   //alias
         VerticalBox {
             Text { text: "Hello power of 2: " + counter; }
@@ -11,5 +13,11 @@ slint::slint!{
 }
 
 fn main() {
-    println!("Hello, world!");
+    let app: App = App::new().unwrap();
+    let weak: Weak<App> = app.as_weak();      //weak pointer so we can move into the closure
+    app.on_clicked(move || {
+        let app: App = weak.upgrade().unwrap();
+        app.set_counter(app.get_counter() * 2);
+    });
+    app.run().unwrap();
 }
