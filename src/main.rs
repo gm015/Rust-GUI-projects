bc373c4c2136d861282857ccbc87b228730e8c26
pdol <79060962+gm015@users.noreply.github.com>
2024-02-12 03:00:04 +0100

    add logic/connect layout to the main()

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,32 @@ use slint::Weak;
 slint::slint!{
     import { VerticalBox } from "std-widgets.slint";    // remove button and add it below - customised
  
-    export global CalcLogic {
+    export global logic_pressed_button {
         callback button-pressed(string);
     }
     component Button {
         in property <string> text;
         Rectangle {
-            background: ta.pressed ? red : ta.has-hover ? #93af93 : #52a1f0;
+            background: ta.pressed ? red : ta.has-hover ? #93af93 : #adb1b6;
             animate background { duration: 100ms; }
             border-radius: 4px;
             border-width: 2px;
             border-color: self.background.darker(20%);
             ta := TouchArea {
-                clicked => { CalcLogic.button-pressed(root.text); }
+                clicked => { logic_pressed_button.button-pressed(root.text); }
             }
         }
         Text { text: root.text; }
     }
     export component App inherits Window {
-        in property <int> counter: 2;
+        background: silver;
+        width: 300px;
+        height: 500px;
+        in property <int> value: 2;
         GridLayout {
             padding: 10px;
             spacing: 5px;
-            Text { text: counter; colspan: 3;}
+            Text { text: value; colspan: 3;}
             Row {
                 Button { text: "1";}
                 Button { text: "2";}
@@ -50,7 +53,13 @@ slint::slint!{
 
 fn main() {
     let app: App = App::new().unwrap();
-    let _weak: Weak<App> = app.as_weak();
+    let weak: Weak<App> = app.as_weak();
 
+    app.global::<logic_pressed_button>().on_button_pressed(
+        move |value| {
+        let app = weak.unwrap();
+        app.set_value(value.parse().unwrap());
+        },
+    );
     app.run().unwrap();
 }
