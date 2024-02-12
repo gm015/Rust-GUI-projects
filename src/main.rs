6081f452f11df9a2a5981befeb0760544a702f38
pdol <79060962+gm015@users.noreply.github.com>
2024-02-12 02:32:13 +0100

    customise buttons -> num pad

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use slint::Weak;
 
 slint::slint!{
-    import { Button, VerticalBox } from "std-widgets.slint";
+    import { VerticalBox } from "std-widgets.slint";    // remove button and add it below - customised
+ 
+    component Button {
+        in property <string> text;
+        Rectangle {
+            background: ta.pressed ? red : ta.has-hover ? #93af93 : #52a1f0;
+            border-radius: 4px;
+            border-width: 2px;
+            border-color: self.background.darker(20%);
+            ta := TouchArea {}
+        }
+        Text { text: root.text; }
+    }
     export component App inherits Window {
-        in property <int> counter: 2;        // in -> make it public
+        in property <int> counter: 2;
         GridLayout {
             padding: 20px;
             spacing: 10px;
-            Text { text: "power of 2: " + counter; colspan: 3;}
+            Text { text: counter; colspan: 3;}
             Row {
                 Button { text: "1";}
                 Button { text: "2";}
@@ -32,7 +44,7 @@ slint::slint!{
 
 fn main() {
     let app: App = App::new().unwrap();
-    let weak: Weak<App> = app.as_weak();      //weak pointer so we can move into the closure
+    let weak: Weak<App> = app.as_weak();
 
     app.run().unwrap();
 }
